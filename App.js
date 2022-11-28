6c799e5a2a354c504b4863d975e0de8d732fb4c4
unknown <juanjosesierram@gmail.com>
2022-11-27 20:06:37 -0500

    feat: Added login

--- App.js
+++ App.js
@@ -2,14 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NativeRouter } from 'react-router-native';
 import Main from './src/Main'
+import { Provider } from "react-redux"
+import { store } from './src/store';
 
 export default function App() {
   return (
     <>
-      <NativeRouter>
-        <Main />
-      </NativeRouter>
-      <StatusBar style='auto'/>
+      <Provider store={store}>
+        <NativeRouter>
+          <Main />
+        </NativeRouter>
+        <StatusBar style='auto' />
+      </Provider>
     </>
   );
 }
--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,30 +1,114 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
 import * as Device from "expo-device"
+import { TextInput, View, StyleSheet, Text, Pressable, Alert } from "react-native"
+import * as yup from "yup"
+import { Formik } from "formik"
+
+const formStyle = StyleSheet.create({
+    input: {
+        backgroundColor: "#1C1C1C",
+        marginBottom: 10,
+        marginTop: 10,
+        height: 40,
+        color: "#FFFFFF",
+        width: 350,
+        alignSelf: "center",
+        borderRadius: 30,
+        paddingLeft: 15
+    },
+    placeholder: {
+        paddingLeft: 35,
+        color: "#A9ABB8",
+    },
+    button: {
+        backgroundColor: '#A9ABB8',
+        alignSelf: 'center',
+        alignItems: 'center',
+        margin: 20,
+        borderRadius: 10,
+        paddingTop: 15,
+        width: 250,
+        height: 50
+    }
+})
+
+const validationSchema = yup.object().shape({
+    username: yup.string()
+        .required("Username is required"),
+    password: yup.string()
+        .required("Password is required")
+})
+
+const initialValues = {
+    username: "",
+    password: ""
+}
 
 const LogIn = () => {
 
     const [user, setUser] = useState("")
 
-    const logIn = async () => {
+    const logIn = async (userName, password) => {
 
-        const {data} = await axios({
+        const { data } = await axios({
             method: "get",
             //Si se conecta un dispositivo móvil se conecta por medio de un tunel con ngrok (ngrok 5142)
             url: Device.osName == "Windows" ? "http://localhost:5142/api/consultarIngreso" : "https://cb2b-2800-e2-8880-1c2f-4986-788b-ee72-cec5.ngrok.io/api/consultarIngreso",
             params: {
-                userName: "Sierra",
-                password: "321"
+                userName,
+                password
             }
         })
-        setUser(data)
+        return data.result
     }
 
-    useEffect(() => {
-        logIn()
-    }, [])
+    const onSubmit = async ({  username, password }) => {
+        const a = await logIn(username, password)
+        if (a === true) console.log("Successfull")
+        else onInvalidData("Incorrect username or password")
+    }
 
-    console.log(user);
+    const onInvalidData = (message) =>
+        Alert.alert(
+            "Invalid Inputs",
+            message,
+            [
+                {
+                    text: "Ok",
+                    style: "cancel"
+                }
+            ]
+        )
+
+    return (
+        <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+            {({ handleSubmit, handleChange, handleBlur, values, isValid }) => (
+                <View>
+                <Text style={formStyle.placeholder}>Username</Text>
+                <TextInput
+                    onChangeText={handleChange('username')}
+                    style={formStyle.input}
+                    name="username"
+                    value={values.username}
+                    onBlur={handleBlur('username')}
+                />
+                <Text style={formStyle.placeholder}>Password</Text>
+                <TextInput
+                    onChangeText={handleChange('password')}
+                    style={formStyle.input}
+                    name="password"
+                    secureTextEntry={true}
+                    value={values.password}
+                    onBlur={handleBlur('password')}
+                />
+                <Pressable style={formStyle.button} onPress={isValid ? handleSubmit : () => onInvalidData("Both username and password are required")} title="log in">
+                    <Text>Log in</Text>
+                </Pressable>
+            </View>
+            )}
+        </Formik>
+    )
 }
 
 export default LogIn
